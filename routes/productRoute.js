b2cd2c24157f1230e795e1736d13fd38719efeeb
Mohith Chowdary <mohitvasi13211gmail.com>
2026-03-11 13:51:19 +0530

    Implement authentication and user management features with controllers and routes

---
+++ routes/productRoute.js
@@ -0,0 +1,30 @@
+// import { getProducts,addProductForm,addProduct } from "../controllers/productController.js";
+// import express from "express";
+// const productRouter = express.Router();
+
+// productRouter.get("/", getProducts);
+// productRouter.get("/add", addProductForm)
+// productRouter.post("/add", addProduct)
+
+
+// export default productRouter
+
+import {
+  getProducts,
+  addProductForm,
+  addProduct,
+  deleteProduct,
+  editProductForm,
+  saveProduct,
+} from "../controllers/productController.js";
+import express from "express";
+const productRouter = express.Router();
+
+productRouter.get("/", getProducts);
+productRouter.get("/add", addProductForm);
+productRouter.post("/add", addProduct);
+productRouter.get("/:id/delete", deleteProduct);
+productRouter.get("/:id/edit", editProductForm);
+productRouter.post("/:id/save", saveProduct);
+
+export default productRouter;
